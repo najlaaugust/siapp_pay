bc280a39e9fbdd114ad1b3d108230d39e0974398
unknown <anajla@Behemoth.ad.isr.umich.edu>
2016-09-22 14:00:26 -0400

    adding almost all methods

--- src/dbguy.php
+++ src/dbguy.php
@@ -18,7 +18,7 @@ class dbguy {
 		$this->connectionInfo = array("UID"=>$uid, "PWD"=>$pwd, "Database"=>$database);
 	}
 	
-	private function getData($theQuery)
+	private function getData($theQuery, array $params)
 	{
 		$conn = sqlsrv_connect( $this->serverName, $this->connectionInfo);
 		if( $conn === false )
@@ -27,7 +27,7 @@ class dbguy {
 			die( print_r( sqlsrv_errors(), true));
 		}	
 
-		$stmt = sqlsrv_query( $conn, $theQuery);
+		$stmt = sqlsrv_query( $conn, $theQuery, $params);
 		if( $stmt === false )
 		{
 			echo "Error in executing query.</br>";
@@ -42,16 +42,121 @@ class dbguy {
 		return $row;
 	}
 	
+	private function insertData($theQuery, array $params)
+	{
+		$conn = sqlsrv_connect( $this->serverName, $this->connectionInfo);
+		if( $conn === false )
+		{
+			echo "Unable to connect.</br>";
+			die( print_r( sqlsrv_errors(), true));
+		}
+		
+		$stmt = sqlsrv_query( $conn, $theQuery, $params);
+		if( $stmt === false ) {
+			die( print_r( sqlsrv_errors(), true));
+		}
+
+		sqlsrv_free_stmt( $stmt );
+		sqlsrv_close( $conn);
+	}	
+	
+	private function updateData($theQuery, array $params)
+	{	
+		$conn = sqlsrv_connect( $this->serverName, $this->connectionInfo);
+		if( $conn === false )
+		{
+			echo "Unable to connect.</br>";
+			die( print_r( sqlsrv_errors(), true));
+		}
+				
+		$reslt = sqlsrv_query($conn, $theQuery, $params);
+		if($reslt === false )
+			die( FormatErrors( sqlsrv_errors() ) );
+		
+		sqlsrv_free_stmt( $reslt );
+		sqlsrv_close( $conn );
+	}	
+		
+	
 	public function getUserInfo($ConfirmNum, $LastName) {
 		 $tsql = "SELECT 	a.*,  s.state_name AS StateName, cn.country_name AS CountryName
 					 FROM 	dbo.web_applications AS a LEFT OUTER JOIN us_states AS s ON
 					 UPPER(a.[state])	= s.state_id LEFT OUTER JOIN dbo.countries AS cn ON
 					 UPPER(a.country)	= cn.country_id
-					 WHERE	UPPER(confirm_num) 	= '$ConfirmNum' AND
-					 LOWER(lname) 		= '$LastName'";
+					 WHERE	UPPER(confirm_num) 	= ? AND
+					 LOWER(lname) 		= ?";
 
-		$row = $this->getData($tsql);
-		 
+		$row = $this->getData($tsql, array($ConfirmNum, $LastName));
         return $row;
+	}
+	
+	public function getRegInfo($OnlineID) {
+		$tsql = "SELECT 	a.OnlineID, a.Confirm_Num, a.fname, a.mname, a.lname, 
+			        		a.address1, a.address2, a.address3, a.city, a.[state], 
+			                a.zip, a.country, a.hphone, a.email, a.PayAmount,
+			                s.state_name AS StateName, cn.country_name AS CountryName,
+			                (SELECT CAST([value] AS INT) FROM dbo.app_config WHERE [name] = 'current_app_year') AS CurrentYear
+			        FROM 	dbo.web_applications AS a LEFT OUTER JOIN us_states AS s ON
+			                UPPER(a.[state])	= s.state_id LEFT OUTER JOIN dbo.countries AS cn ON
+			                UPPER(a.country)	= cn.country_id
+			        WHERE	a.OnlineID	= ?";
+        $row = $this->getData($tsql, array($OnlineID));
+        return $row;			        		
+	}
+	
+	public function insertWebPayFlowLog(array $data) {
+		
+		//$transaction_id = $data['transaction_id'];
+		//$result_code = $data['result_code'];
+		//$result_msg = $data['result_msg'];
+		//$transaction_amount = $data['transaction_amount'];
+		//$confirm_num = $data['confirm_num'];
+		//$transaction_date = $data['transaction_date'];		
+		
+ 	 	//$tsql = "INSERT INTO web_payflow_log (transaction_id, result_code, result_msg, transaction_amount, confirm_num, transaction_date)
+		 //			VALUES ('$transaction_id', '$result_code', '$result_msg', '$transaction_amount', '$confirm_num', '$transaction_date')";
+
+		$tsql = "INSERT INTO web_payflow_log (transaction_id, result_code, result_msg, transaction_amount, confirm_num, transaction_date) 
+					VALUES (?, ?, ?, '?, ?, ?)";
+		
+ 	 	$this->insertData($tsql, $data);
+		 		
+	}
+	
+	public function updateWebApplications(array $data) {
+
+		/*$CCNumber = $data['CCNumber'];
+		$CCType = $data['CCType'];
+		$CCDate = $data['CCDate'];
+		$CCName = $data['CCName'];
+		$CCEmail = $data['CCEmail'];
+		$CCPhone = $data['CCPhone'];
+		$CCAddress = $data['CCAddress'];
+		$PaymentStat = $data['PaymentStat'];
+		$Confirm_Num = $data['Confirm_Num'];
+		
+		
+		$tsql = "UPDATE 	web_applications SET					 	
+					CCNumber		= '$CCNumber',
+					CCType			= '$CCType',
+					CCDate			= '$CCDate',
+					CCName 			= '$CCName',
+					CCEmail 		= '$CCEmail',
+					CCPhone			= '$CCPhone',
+					CCAddress		= '$CCAddress',
+					PaymentStat		= '$PaymentStat'
+					WHERE 	Confirm_Num		= '$Confirm_Num'";*/
+		$tsql = "UPDATE 	web_applications SET
+					CCNumber		= ?,
+					CCType			= ?,
+					CCDate			= ?,
+					CCName 			= ?,
+					CCEmail 		= ?,
+					CCPhone			= ?,
+					CCAddress		= ?,
+					PaymentStat		= ?'
+					WHERE 	Confirm_Num		= ?";		
+		
+		$this->updateData($tsql, $data);
 	}
 }
--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-$app->get('/isvalid', function ($request, $response) {
+$app->get('/isvalid', function ($request, $response, $args) {
 	$this->logger->addInfo("Check if exists in database");
 	
 	$ConfirmNum ="HWAN-2016-0140";
@@ -14,7 +14,70 @@ $app->get('/isvalid', function ($request, $response) {
 		echo "nothing";
 	else
 		echo "something";
-	
+
 	$response->getBody()->write("yes");
 	return $response;
+});
+
+$app->get('/reginfo/{OnlineID}', function ($request, $response, $args) {
+	$this->logger->addInfo("get registration info");
+
+	//$OnlineID ="200";
+	$OnlineID = (int)$args['OnlineID'];
+
+	//query reg info
+	$regInfo = $this->dbguy->getRegInfo($OnlineID);
+
+	if (empty($regInfo))
+		echo "nothing";
+		else
+				print_r($regInfo);
+
+	$response->getBody()->write("yes");
+	return $response;
+});
+
+
+$app->post('/webpay/new', function ($request, $response) {
+	$this->logger->addInfo("Add payment record to database");
+	
+	$data = $request->getParsedBody();
+	
+	$payment_data = [];
+	$payment_data['transaction_id'] = filter_var($data['PNREF'], FILTER_SANITIZE_STRING);
+	$payment_data['result_code'] = filter_var($data['RESULT'], FILTER_SANITIZE_STRING);	
+	$payment_data['result_msg'] = filter_var($data['RESPMSG'], FILTER_SANITIZE_STRING);
+	$payment_data['transaction_amount'] = filter_var($data['AMOUNT'], FILTER_SANITIZE_STRING);
+	$payment_data['confirm_num'] = filter_var($data['CUSTID'], FILTER_SANITIZE_STRING);
+	$payment_data['transaction_date'] = new DateTime();
+
+
+
+});
+
+$app->post('/registration/update', function($request, $response) {
+	$this->logger->addInfo("Update registration record in database");
+	
+	$data = $request->getParsedBody();
+	
+	$payment_data = [];
+	$payment_data['CCNumber'] = filter_var($data['PNREF'], FILTER_SANITIZE_STRING);
+	$payment_data['CCType'] = "PayPal";
+	$payment_data['CCDate'] = new DateTime();
+	$payment_data['CCName'] = filter_var($data['NAME'], FILTER_SANITIZE_STRING);
+	$payment_data['CCEmail'] = filter_var($data['EMAIL'], FILTER_SANITIZE_STRING);
+	$payment_data['CCPhone'] = filter_var($data['PHONE'], FILTER_SANITIZE_STRING);
+	
+	$address = filter_var($data['ADDRESS'], FILTER_SANITIZE_STRING);
+	$city = filter_var($data['CITY'], FILTER_SANITIZE_STRING);
+	$state = filter_var($data['STATE'], FILTER_SANITIZE_STRING);
+	$zip = filter_var($data['ZIP'], FILTER_SANITIZE_STRING);
+	$country = filter_var($data['COUNTRY'], FILTER_SANITIZE_STRING);
+	
+	$payment_data['CCAddress'] = sprintf("%1$s %2$s, %3$s %4$s %5$s", $address, $city, $state, $zip, $country);
+	$payment_data['PaymentStat'] = "PAID";
+	
+	$payment_data['Confirm_Num'] = filter_var($data['CUSTID'], FILTER_SANITIZE_STRING);
+	
+
 });
